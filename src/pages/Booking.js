c29e1f652f82cbc492b7dc396f47c809e92a12ba
rasyid.wijaya <rasyid.wijaya@shopee.com>
2022-09-13 13:01:29 +0700

    fix booking

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -9,7 +9,7 @@ export default function Booking() {
   const token = `Bearer ${localStorage.getItem('token')}`;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [photosRetrieved, setPhotosRetrieved] = useState(false);
-  const uRL = 'https://papikos-api.herokuapp.com/';
+  const uRL = 'https://papikos-web.herokuapp.com/';
 
   const config = {
     headers: {
@@ -110,9 +110,10 @@ export default function Booking() {
           setIsBooked(true);
           setReservationId(response.data.data.bookings_data[i].id);
           setTotalPrice(response.data.data.bookings_data[i].total_price);
+          setHouseChecked(true);
+          return;
         }
       }
-      setHouseChecked(true);
     });
   }, []);
 
@@ -153,8 +154,8 @@ export default function Booking() {
   }, [eDateSelected]);
 
   const body = JSON.stringify({
-    check_in_date: startDate,
-    check_out_date: endDate,
+    check_in_date: startDate.toString(),
+    check_out_date: endDate.toString(),
   });
 
   useEffect(() => {
@@ -207,132 +208,132 @@ export default function Booking() {
   }, [code]);
 
   return (
-    <div>
-      <GeneralHeader />
-      {
-        firstId !== 0 && houseChecked ? (
-          <div>
-            <div className="d-flex justify-content-center">
-              <div className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner">
-                  {
-                    photos.map((photo) => (
-                      <div className={counter === photo.id ? 'carousel-item active' : 'carousel-item'} key={photo.id}>
-                        <img className="d-block img-fluid mx-auto" src={photo.photo_url} alt="" style={{ width: '500px', height: '375px' }} />
-                      </div>
-                    ))
-                  }
-                </div>
-              </div>
-            </div>
-            <div className="d-flex mt-5 row">
-              <div className="d-flex col-6 justify-content-end"><button className="btn btn-info" type="button" onClick={handlePrev}> Prev  &nbsp; &lt; </button></div>
-              <div className="d-flex col-6"><button className="btn btn-info" type="button" onClick={handleNext}> &gt; &nbsp; Next </button></div>
-            </div>
-            <div className="d-flex my-5 row justify-content-center">
-              <div className="col-8 col-xs-8 col-sm-8 col-md-8 col-lg-4 border border-1 border-info rounded-2 p-3">
-                {
-                  isBooked ? (
-                    <div className="p-0 m-0" style={{ textAlign: 'center' }}>
-                      <p>Already booked,</p>
+      <div>
+        <GeneralHeader />
+        {
+          firstId !== 0 && houseChecked ? (
+              <div>
+                <div className="d-flex justify-content-center">
+                  <div className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
                       {
-                        isPaid ? (
-                          <div>
-                            <p>
-                              and already
-                              {' '}
-                              <b> paid </b>
-                              <br />
-                              {' '}
-                            </p>
-                            <p>Visit bookings to re-check booking code and information:</p>
+                        photos.map((photo) => (
+                            <div className={counter === photo.id ? 'carousel-item active' : 'carousel-item'} key={photo.id}>
+                              <img className="d-block img-fluid mx-auto" src={photo.photo_url} alt="" style={{ width: '500px', height: '375px' }} />
+                            </div>
+                        ))
+                      }
+                    </div>
+                  </div>
+                </div>
+                <div className="d-flex mt-5 row">
+                  <div className="d-flex col-6 justify-content-end"><button className="btn btn-info" type="button" onClick={handlePrev}> Prev  &nbsp; &lt; </button></div>
+                  <div className="d-flex col-6"><button className="btn btn-info" type="button" onClick={handleNext}> &gt; &nbsp; Next </button></div>
+                </div>
+                <div className="d-flex my-5 row justify-content-center">
+                  <div className="col-8 col-xs-8 col-sm-8 col-md-8 col-lg-4 border border-1 border-info rounded-2 p-3">
+                    {
+                      isBooked ? (
+                          <div className="p-0 m-0" style={{ textAlign: 'center' }}>
+                            <p>Already booked,</p>
+                            {
+                              isPaid ? (
+                                  <div>
+                                    <p>
+                                      and already
+                                      {' '}
+                                      <b> paid </b>
+                                      <br />
+                                      {' '}
+                                    </p>
+                                    <p>Visit bookings to re-check booking code and information:</p>
+                                  </div>
+                              ) : (
+                                  <div style={{ textAlign: 'center' }}>
+                                    <p>
+                                      <b>pay</b>
+                                      {' '}
+                                      to get the booking code.
+                                    </p>
+                                    <p>
+                                      Total fee:
+                                      {' '}
+                                      {' '}
+                                      {' '}
+                                      {formatter.format(totalPrice).replace('Rp', 'IDR')}
+                                    </p>
+                                    <button className="btn btn-success mb-3" type="button" onClick={handlePayment}>Pay</button>
+                                  </div>
+                              )
+                            }
+                            {
+                              !isPaid ? <p>Visit bookings:</p> : ''
+                            }
+                            <button className="btn btn-warning" type="button" onClick={handleToBookings}>Bookings</button>
                           </div>
-                        ) : (
+                      ) : (
                           <div style={{ textAlign: 'center' }}>
-                            <p>
-                              <b>pay</b>
-                              {' '}
-                              to get the booking code.
-                            </p>
-                            <p>
-                              Total fee:
-                              {' '}
-                              {' '}
-                              {' '}
-                              {formatter.format(totalPrice).replace('Rp', 'IDR')}
-                            </p>
-                            <button className="btn btn-success mb-3" type="button" onClick={handlePayment}>Pay</button>
+                            <div className="mb-2">
+                              <input className="mb-xs-2 mb-sm-2 mr-md-2 mr-lg-2" type="date" onChange={handleSelectStart} />
+                              <input type="date" onChange={handleSelectEnd} />
+                            </div>
+                            <button className="btn btn-info w-25" type="button" onClick={(handleBook)}>Book</button>
                           </div>
-                        )
-                      }
-                      {
-                        !isPaid ? <p>Visit bookings:</p> : ''
-                      }
-                      <button className="btn btn-warning" type="button" onClick={handleToBookings}>Bookings</button>
-                    </div>
-                  ) : (
-                    <div style={{ textAlign: 'center' }}>
-                      <div className="mb-2">
-                        <input className="mb-xs-2 mb-sm-2 mr-md-2 mr-lg-2" type="date" onChange={handleSelectStart} />
-                        <input type="date" onChange={handleSelectEnd} />
-                      </div>
-                      <button className="btn btn-info w-25" type="button" onClick={(handleBook)}>Book</button>
-                    </div>
-                  )
-                  }
-              </div>
-            </div>
-            {
-              !loadHouses ? (
-                <div className="mb-5">
-                  <div className="row mb-5">
-                    <div className="d-flex col-12 justify-content-center">
-                      <p>
-                        {houseData.house_name}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="row mb-5 justify-content-center">
-                    <div className="d-flex col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4 justify-content-between">
-                      <p>
-                        City:
-                      </p>
-                      <p className="justify-content-end">
-                        {houseData.city_name}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="row mb-5 justify-content-center">
-                    <div className="d-flex col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4 justify-content-between">
-                      <p>
-                        Price per Night:
-                      </p>
-                      <p className="justify-content-end">
-                        {formatter.format(houseData.price_per_night).replace('Rp', 'IDR')}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="row mb-5 justify-content-center">
-                    <div className="d-flex col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4 justify-content-between">
-                      <p>
-                        Max number of guest(s):
-                      </p>
-                      <p className="justify-content-end">
-                        {houseData.max_guest}
-                      </p>
-                    </div>
-                  </div>
-                  <p className="d-flex justify-content-center">Description</p>
-                  <div className="row justify-content-center">
-                    <div className="col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4"><p className="p=5" style={{ textAlign: 'justify' }}>{houseData.description}</p></div>
+                      )
+                    }
                   </div>
                 </div>
-              ) : <div><h3 className="d-flex justify-content-center">Loading house...</h3></div>
-            }
-          </div>
-        ) : <div className="d-flex justify-content-center mt-5"><h2>Loading house...</h2></div>
-      }
-      <Footer />
-    </div>
+                {
+                  !loadHouses ? (
+                      <div className="mb-5">
+                        <div className="row mb-5">
+                          <div className="d-flex col-12 justify-content-center">
+                            <p>
+                              {houseData.house_name}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="row mb-5 justify-content-center">
+                          <div className="d-flex col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4 justify-content-between">
+                            <p>
+                              City:
+                            </p>
+                            <p className="justify-content-end">
+                              {houseData.city_name}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="row mb-5 justify-content-center">
+                          <div className="d-flex col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4 justify-content-between">
+                            <p>
+                              Price per Night:
+                            </p>
+                            <p className="justify-content-end">
+                              {formatter.format(houseData.price_per_night).replace('Rp', 'IDR')}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="row mb-5 justify-content-center">
+                          <div className="d-flex col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4 justify-content-between">
+                            <p>
+                              Max number of guest(s):
+                            </p>
+                            <p className="justify-content-end">
+                              {houseData.max_guest}
+                            </p>
+                          </div>
+                        </div>
+                        <p className="d-flex justify-content-center">Description</p>
+                        <div className="row justify-content-center">
+                          <div className="col-8 col-xs-8 col-sm-8 col-md-4 col-lg-4"><p className="p=5" style={{ textAlign: 'justify' }}>{houseData.description}</p></div>
+                        </div>
+                      </div>
+                  ) : <div><h3 className="d-flex justify-content-center">Loading house...</h3></div>
+                }
+              </div>
+          ) : <div className="d-flex justify-content-center mt-5"><h2>Loading house...</h2></div>
+        }
+        <Footer />
+      </div>
   );
 }
